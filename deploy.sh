64054d05826d3befec7a575e1f7d7ad80de8c612
Antonio García-Domínguez <nyoescape@gmail.com>
2015-11-11 12:26:44 +0000

    Add Tycho build and start integrating with Travis

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Exit immediately if something goes wrong
+set -o errexit
+
+# Run the regular build
+mvn --quiet install
+
+# Clone the gh-pages branch
+rm -rf out || true
+git clone -b gh-pages --single-branch https://github.com/bluezio/ecore2thrift
+cd out
+
+# Indicate clearly that this commit comes from Travis
+git config user.name "Travis CI"
+git config user.email "antonio.garciadominguez@york.ac.uk"
+
+# If the tip comes from Travis, amend it. Otherwise, add a new commit.
+rm -rf hawk-updates
+cp -r ../uk.ac.york.mondo.ecore2thrift.updatesite/target/repository ecore2thrift-updates
+git add --all .
+if git log --format=%an HEAD~.. | grep -q "Travis CI"; then
+    COMMIT_FLAGS="--amend"
+fi
+git commit $COMMIT_FLAGS -am "Build update site"
+
+# Force push to the gh-pages branch
+git push --force "https://${GH_TOKEN}@${GH_REF}" gh-pages

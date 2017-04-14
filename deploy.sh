f8fd8190a10e55bf1c623114e78186bac2c1fc23
Antonio García-Domínguez <nyoescape@gmail.com>
2017-04-14 18:46:21 +0100

    Keep refining deploy script

--- deploy.sh
+++ deploy.sh
@@ -29,9 +29,6 @@ cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
 # Run our compile script
 doCompile
 
